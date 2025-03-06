e8a0e8af1771014728069772ddd090ae78177f59
ZaretaR <>
2025-03-06 09:56:31 +0100

    Layout | Changes layout login blade

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -74,7 +74,7 @@ new #[Layout('components.layouts.auth')] class extends Component {
 }; ?>
 
 <div class="flex flex-col gap-6">
-    <x-auth-header title="Log in to your account" description="Enter your email and password below to log in" />
+    <x-auth-header title="TunityTogglView" description="Log in and get tracking!" />
 
     <!-- Session Status -->
     <x-auth-session-status class="text-center" :status="session('status')" />
@@ -83,13 +83,12 @@ new #[Layout('components.layouts.auth')] class extends Component {
         <!-- Email Address -->
         <flux:input
             wire:model="email"
-            :label="__('Email address')"
+            :label="__('Email')"
             type="email"
             name="email"
             required
             autofocus
             autocomplete="email"
-            placeholder="email@example.com"
         />
 
         <!-- Password -->
@@ -101,12 +100,11 @@ new #[Layout('components.layouts.auth')] class extends Component {
                 name="password"
                 required
                 autocomplete="current-password"
-                placeholder="Password"
             />
 
             @if (Route::has('password.request'))
                 <flux:link class="absolute right-0 top-0 text-sm" :href="route('password.request')" wire:navigate>
-                    {{ __('Forgot your password?') }}
+                    {{ __('Reset password?') }}
                 </flux:link>
             @endif
         </div>
@@ -114,15 +112,19 @@ new #[Layout('components.layouts.auth')] class extends Component {
         <!-- Remember Me -->
         <flux:checkbox wire:model="remember" :label="__('Remember me')" />
 
-        <div class="flex items-center justify-end">
-            <flux:button variant="primary" type="submit" class="w-full">{{ __('Log in') }}</flux:button>
+        <div class="flex justify-center gap-5">
+            <div class="flex items-center justify-end">
+                <flux:button variant="primary" type="submit" class="w-full">{{ __('Log in via email') }}</flux:button>
+            </div>
+            <div class="flex items-center justify-end">
+                <flux:button variant="primary" type="submit" class="w-full">{{ __('Google') }}</flux:button>
+            </div>
         </div>
     </form>
 
     @if (Route::has('register'))
       <div class="space-x-1 text-center text-sm text-zinc-600 dark:text-zinc-400">
-          Don't have an account?
-          <flux:link :href="route('register')" wire:navigate>Sign up</flux:link>
+          <flux:link :href="route('register')" wire:navigate>Sign up for an account</flux:link>
       </div>
     @endif
 </div>
--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::redirect('/', '/login')->name('home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::middleware(['auth'])->group(function () {
+// Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth', 'ensure.email.domain'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
