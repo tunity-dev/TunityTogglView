8ccec46393e38cc2e3be9f5fe029080127ea64bc
ZaretaR <>
2025-03-06 13:49:42 +0100

    Layout | Top users added

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -10,24 +10,18 @@
             <a href="{{ route('dashboard') }}" class="mr-5 flex items-center space-x-2" wire:navigate>
                 <x-app-logo />
             </a>
-
+            
             <flux:navlist variant="outline">
                 <flux:navlist.group heading="Platform" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
 
-            <flux:spacer />
-
             <flux:navlist variant="outline">
-                <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
-                {{ __('Repository') }}
-                </flux:navlist.item>
-
-                <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits" target="_blank">
-                {{ __('Documentation') }}
-                </flux:navlist.item>
+                <flux:navlist.item icon="star" :href="route('top-workers')" :current="request()->routeIs('top-workers')" wire:navigate>{{ __('Top workers') }}</flux:navlist.item>
             </flux:navlist>
+
+            <flux:spacer />
 
             <!-- Desktop User Menu -->
             <flux:dropdown position="bottom" align="start">
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Livewire\Volt\Volt;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TopUserController;
 use App\Http\Controllers\CompleteRegistrationController;
 
 Route::redirect('/', '/login')->name('home');
@@ -12,6 +13,8 @@ Route::view('dashboard', 'dashboard')
 
 Route::get('/complete-registration', [CompleteRegistrationController::class, 'showForm'])->name('complete.registration');
 Route::post('/complete-registration', [CompleteRegistrationController::class, 'saveDetails']);
+
+Route::get('/top-workers', [TopUserController::class, 'index'])->name('top-workers');
 
 // Route::middleware(['auth'])->group(function () {
 Route::middleware(['auth', 'ensure.email.domain'])->group(function () {
